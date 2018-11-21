31967ccf50d3af58a1253c9d4fa0ea8325fae57a
Zach Grusznsk <zachg@Zachs-Air.attlocal.net>
2018-11-21 12:36:43 -0600

    changed acces headers to env vars

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', process.env.URL);
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization, csrf, test'
